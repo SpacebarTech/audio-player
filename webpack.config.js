5c394e5dedc45cb8e4515f4e7478519495189444
Tommyduong23 <tommyduong23@gmail.com>
2018-09-05 10:56:28 -0700

    fix(output): output file changed to index

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require( 'path' );
 module.exports = {
   entry: path.resolve( __dirname, 'src/main.vue' ),
   output: {
-    filename: 'audioPlayer.js',
+    filename: 'index.js',
 		path: path.resolve( __dirname, 'dist' ),
 		library: 'audioPlayer',
 		libraryTarget: 'umd',
